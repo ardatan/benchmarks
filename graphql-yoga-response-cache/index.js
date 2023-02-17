5a89b0bc2e65ff9672e9bc830044b493755148a7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-02-17 12:27:58 +0300

    More scenarios

---
+++ graphql-yoga-response-cache/index.js
@@ -0,0 +1,36 @@
+import { createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+import { gateway } from "../common/gateway.js";
+import { useApolloFederation } from "@envelop/apollo-federation";
+import { serviceSchemas } from "../common/service-schemas.js";
+import waitOn from "wait-on";
+import { useResponseCache } from "@envelop/response-cache";
+
+for (let i = 0; i < serviceSchemas.length; i++) {
+  const schema = serviceSchemas[i];
+  const port = 8001 + i;
+  const yoga = createYoga({
+    schema,
+    logging: false,
+  });
+  const server = createServer(yoga);
+  await new Promise((resolve) => server.listen(port, resolve));
+}
+await waitOn({ resources: ["tcp:8001", "tcp:8002", "tcp:8003", "tcp:8004"] });
+await gateway.load();
+const yoga = createYoga({
+  plugins: [
+    useApolloFederation({
+      gateway,
+    }),
+    useResponseCache({
+        session: () => null,
+    }),
+  ],
+  maskedErrors: false,
+  logging: false,
+});
+
+const server = createServer(yoga);
+
+server.listen(8000);
